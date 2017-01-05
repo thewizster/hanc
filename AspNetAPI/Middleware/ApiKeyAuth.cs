847239e6c5ca563e78e4dafd6601fedd2876d1a6
Raymond Brady <thewizster@gmail.com>
2017-01-05 12:59:46 -0600

    support api path and auth token

--- AspNetAPI/Middleware/ApiKeyAuth.cs
+++ AspNetAPI/Middleware/ApiKeyAuth.cs
@@ -2,32 +2,65 @@
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Webextant.Security.Cryptography;
+using Microsoft.Extensions.Primitives;
 
 namespace Hanc.AspNetAPI
 {
+    /// <summary>
+    /// Adapted from concepts used here: https://blogs.msdn.microsoft.com/dotnet/2016/09/19/custom-asp-net-core-middleware-example/
+    /// </summary>
     public class ApiKeyMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly string _apiKey;
-        public ApiKeyMiddleware(RequestDelegate next, IOptions<SecretOptions> optionsAccessor)
+        private readonly Mac _mac;
+        private readonly string _appId;
+        private readonly bool _requireAuthToken;
+        private readonly string _endpointPath;
+        public ApiKeyMiddleware(RequestDelegate next, IOptions<SecretOptions> optionsAccessor, string path)
         {
             _next = next;
-            _apiKey = optionsAccessor.Value.ApiKey;
+            _mac = new Mac(optionsAccessor.Value.MacSecret);
+            _appId = optionsAccessor.Value.AppId;
+            _requireAuthToken = optionsAccessor.Value.RequireApiAuthToken;
+            _endpointPath = path;
         }
 
         public async Task Invoke(HttpContext context)
         {
-            if (!context.Request.Headers.Keys.Contains("ApiKey"))
+            // If request is for api path then check the application id and auth token
+            if (context.Request.Path.Value.Contains(_endpointPath))
             {
-                context.Response.StatusCode = 400; //Bad Request                
-                await context.Response.WriteAsync("Bad Request");
-                return;
-            }
-            else
-            {
-                if (context.Request.Headers["ApiKey"] != _apiKey)
+                bool _auth = false;
+                bool _appIdIsValid = false;
+                StringValues _clientAppId;
+                StringValues _clientAuthToken;
+
+                context.Request.Headers.TryGetValue("X-Hanc-Application-Id", out _clientAppId);
+                context.Request.Headers.TryGetValue("X-Hanc-Auth-Token", out _clientAuthToken);
+
+                if (_clientAppId == StringValues.Empty)
                 {
-                    context.Response.StatusCode = 401; //UnAuthorized
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Bad request");
+                    return;
+                }
+
+                // check the application id
+                _appIdIsValid = _clientAppId == _appId;
+
+                // Check auth token if required
+                if (_requireAuthToken)
+                {
+                    _auth = _clientAuthToken == _mac.GenerateToken(_clientAppId) && _appIdIsValid;
+                }
+                else {
+                    _auth = _appIdIsValid;
+                }
+
+                if (!_auth)
+                {
+                    context.Response.StatusCode = 401;
                     await context.Response.WriteAsync("Unauthorized");
                     return;
                 }
@@ -39,9 +72,9 @@ namespace Hanc.AspNetAPI
     }
     public static class UserKeyValidatorsExtension
     {
-        public static IApplicationBuilder UseApiKeyAuthorization(this IApplicationBuilder app)
+        public static IApplicationBuilder UseApiKeyAuthorization(this IApplicationBuilder app, string path)
         {
-            app.UseMiddleware<ApiKeyMiddleware>();
+            app.UseMiddleware<ApiKeyMiddleware>(path);
             return app;
         }
     }
